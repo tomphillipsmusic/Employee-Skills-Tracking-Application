48bcc219e1c7819262747a1de3a95b7f07efee5b
Tom Phillips <tomphillipsmusic@gmail.com>
2019-11-27 16:04:55 -0500

    created project with Spring Boot

---
+++ employee-skills-tracking-application/src/main/java/com/techelevator/employeeskillstrackingapplication/EmployeeSkillsTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.techelevator.employeeskillstrackingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSkillsTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSkillsTrackingApplication.class, args);
+	}
+
+}
---
+++ employee-skills-tracking-application/src/test/java/com/techelevator/employeeskillstrackingapplication/EmployeeSkillsTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techelevator.employeeskillstrackingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSkillsTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
