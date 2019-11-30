15c7b281008e8e3813622065c4bfce20c3287c36
Tom Phillips <tomphillipsmusic@gmail.com>
2019-11-30 16:36:48 -0500

    created vue app for front end

---
+++ employee-skills-tracking-webapp/src/components/EmployeeTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="employee-table">
+    <h1>Perficient Employees</h1>
+    <table>
+      <thead>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>Position</td>
+      </thead>
+      <tr>
+        <td>Tom</td>
+        <td>Phillips</td>
+        <td>Associate Developer</td>
+      </tr>
+      <tr>
+        <td>Al</td>
+        <td>Gilson</td>
+        <td>Software Developer</td>
+      </tr>
+            <tr>
+        <td>Robin</td>
+        <td>Harris</td>
+        <td>Senior Develoepr</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ employee-skills-tracking-webapp/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <employee-table></employee-table>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import EmployeeTable from "@/components/EmployeeTable.vue";
+
+export default {
+  name: "home",
+  components: {
+    EmployeeTable,
+    HelloWorld
+  },
+  data() {
+    return {
+      apiURL: "http://localhost:8080/employees/",
+      employees: []
+    };
+  },
+  methods: {
+    getEmployees() {
+      fetch(this.apiURL)
+        .then(response => {
+          return response.json();
+        })
+        .then(employees => {
+          this.employees = employees;
+        })
+        .catch(err => console.log(err));
+      console.log(this.employees);
+    }
+  }
+};
+</script>
