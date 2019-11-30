8c2b89a2eb5a495cc587902b727ad8c3586e40e9
Tom Phillips <tomphillipsmusic@gmail.com>
2019-11-29 19:53:50 -0500

    got h2 database working for many to many relationship

--- employee-skills-tracking-application/employee-skills-tracking-application/src/main/resources/data.sql
+++ employee-skills-tracking-application/employee-skills-tracking-application/src/main/resources/data.sql
@@ -1,2 +1,10 @@
+INSERT INTO employee(first_name, last_name, position) VALUES('Tom', 'Phillips', 'Associate Developer');
+INSERT INTO employee(first_name, last_name, position) VALUES('Al', 'Gilson', 'Software Developer');
+INSERT INTO employee(first_name, last_name, position) VALUES('John', 'Livecchi', 'Senior Developer');
 
+INSERT INTO skill(description, name, rating) VALUES ('Able to communicate ideas efficiently', 'Communication', 5);
+INSERT INTO skill(description, name, rating) VALUES ('Confident with speaking to large groups', 'Public Speaking', 3);
+INSERT INTO skill(description, name, rating) VALUES ('Ability to teach others', 'Mentoring', 2);
+
+INSERT into employee_skill(employee_id, skill_id) VALUES (1, 1);
 commit;
